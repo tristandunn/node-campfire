2750f592d2d4248f92534d3193ba3dd92002addb
Brian Donovan <me@brian-donovan.com>
2011-02-23 10:33:10 -0800

    Don't use global initialization, allows multiple connections per process.

--- examples/growl.js
+++ examples/growl.js
@@ -1,38 +1,38 @@
 var system   = require('sys');
 var Campfire = require('../lib/campfire').Campfire;
 
-Campfire.initialize({
+var yourCampfire = new Campfire({
   token   : 'YOUR_TOKEN',
   account : 'YOUR_ACCOUNT'
 });
 
-var
-room = Campfire.Room(ROOM_ID);
-room.join(function() {
-  room.listen(function(message) {
-    // Ignore emotes, sounds, timestamps, etc.
-    if (message.type != 'TextMessage') {
-      return;
-    }
+yourCampfire.room(ROOM_ID_OR_NAME, function(room) {
+  room.join(function() {
+    room.listen(function(message) {
+      // Ignore emotes, sounds, timestamps, etc.
+      if (message.type != 'TextMessage') {
+        return;
+      }
 
-    // Ignore your own messages.
-    // if (message.user.name == 'YOUR_NAME') {
-    //   return;
-    // }
+      // Ignore your own messages.
+      // if (message.user.name == 'YOUR_NAME') {
+      //   return;
+      // }
 
-    // Only notify on keywords.
-    // var keywords = ['YOUR_NAME', 'URGENT', 'Whatever'];
-    //
-    // if (!message.body.match(new RegExp(keywords.join('|')))) {
-    //   return;
-    // }
+      // Only notify on keywords.
+      // var keywords = ['YOUR_NAME', 'URGENT', 'Whatever'];
+      //
+      // if (!message.body.match(new RegExp(keywords.join('|')))) {
+      //   return;
+      // }
 
-    var
-    command = '/usr/local/bin/growlnotify';
-    command += ' -t "' + message.user.name + '"';
-    command += ' -m "' + message.body.replace('"', "'") + '"';
-    command += ' --image "examples/images/icon.png"';
+      var
+      command = '/usr/local/bin/growlnotify';
+      command += ' -t "' + message.user.name + '"';
+      command += ' -m "' + message.body.replace('"', "'") + '"';
+      command += ' --image "examples/images/icon.png"';
 
-    system.exec(command);
+      system.exec(command);
+    });
   });
 });
--- examples/ping.js
+++ examples/ping.js
@@ -1,24 +1,24 @@
 var system   = require('sys');
 var Campfire = require('../lib/campfire').Campfire;
 
-Campfire.initialize({
+var yourCampfire = new Campfire({
   token   : 'YOUR_TOKEN',
   account : 'YOUR_ACCOUNT'
 });
 
-var
-room = Campfire.Room(ROOM_ID);
-room.join(function() {
-  room.listen(function(message) {
-    if (message.body == 'PING') {
-      system.puts('PING received.');
+yourCampfire.room(ROOM_ID_OR_NAME, function(room) {
+  room.join(function() {
+    room.listen(function(message) {
+      if (message.body == 'PING') {
+        system.puts('PING received.');
 
-      room.speak('PONG', function(data) {
-        system.puts('PONG sent at ' + data.message.created_at + '.');
-      });
-    } else {
-      system.puts('Received unknown message:');
-      system.puts(system.inspect(message));
-    }
+        room.speak('PONG', function(data) {
+          system.puts('PONG sent at ' + data.message.created_at + '.');
+        });
+      } else {
+        system.puts('Received unknown message:');
+        system.puts(system.inspect(message));
+      }
+    });
   });
 });
