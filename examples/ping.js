e97accee70dc40d9341349d26e214b1638ad465c
Tristan Dunn <tristanzdunn@gmail.com>
2013-12-07 13:47:01 -0600

    Allow room listening to be ended.
    Fixes #13.

--- examples/ping.js
+++ examples/ping.js
@@ -7,13 +7,15 @@ var instance = new Campfire({
 });
 
 instance.join(ROOM_ID, function(error, room) {
-  room.listen(function(message) {
+  var listener = room.listen(function(message) {
     if (message.body == "PING") {
       console.log("PING received.");
 
       room.speak("PONG", function(error, response) {
         console.log("PONG sent at " + response.message.created_at + ".");
       });
+    } else if (message.body == "END") {
+      listener.end();
     } else {
       console.log("Received unknown message:");
       console.log(message);
